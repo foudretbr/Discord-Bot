f91d927755a49431b32b69f738a5502bdc397e00
foudretbr <panier.ikea@gmail.com>
2025-11-28 22:35:31 +0100

    Step up

---
+++ cogs/moderation.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from discord.ext import context
+
+class Moderation(commands.Cog, name="moderation"):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.hybrid_command(name="kick", description="Kick an user out of the serve.")
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, context: context, user: discord.User, *, reason: str = "Not specified") -> None :
+        """Kick an user from the server """
+        member = context.guild.get_member(user.id) or await context.guild.fetch_member(user.id)
+        if member.guild_permissions.administrator:
+            embed = discord.Embed(description="User has administrator permission.", color=0xE02B2B)
+            await context.send(embed=embed)
+        else:
+            try:
+                embed = discord.Embed(description=f"**{member}** was kicked by **{context.author}**!",color=0xBEBEFE,)
+                embed.add_field(name="Reason:", value=reason)
+                await context.send(embed=embed)
+                try:
+                    await member.send(f"You were kicked from **{context.author}** from **{context.guild.name}** !\nReason: {reason}")
+
+                except:
+                    pass
+                await member.kick(reason=reason)
+            except:
+                embed = discord.Embed(description="An error occurred while trying to kick the user. Make sure my role is above the role of the user you want to kick.", color=0xE02B2B)
+                await context.send(embed=embed)
+
+async def setup(bot) -> None:
+    await bot.add_cog(Moderation(bot))
+
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+from discord.ext import context
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+PREFIX = os.getenv('PREFIX', '$')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -7,12 +14,14 @@ intents.members = True
     
 class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix='$', intents=intents)
+        super().__init__(command_prefix=PREFIX, intents=intents)
 
     async def on_ready(self):
+        for file in os.listdir('./cogs'):
+            if file.endswith('.py'):
+                await self.load_extension(f'cogs.{file[:-3]}')
+
         print(f"Bot is ready! Connected to : {self.user}")
-        #ajouter des cogs
-        pass
 
     async def on_member_join(self, member):
         guild = member.guild
@@ -21,11 +30,32 @@ class MyBot(commands.Bot):
             await member.add_roles(role)
             print(f"Rôle '{role.name}' attribué à {member.name}")
 
-@commands.command()
-async def test(ctx, arg):
-    await ctx.channel.send(arg)
+    async def on_message(self, message):
+        if not message.author.bot:
+            if "nigger" in message.content.lower():
+                # await message.delete()
+                await message.channel.send(f'{message.author.mention}, NIGGER !')
+
+        await self.process_commands(message)
+
+# @commands.command()
+# async def test(ctx, arg):
+#     await ctx.channel.send(arg)
+
+# @commands.command()
+# async def channel(ctx):
+#     i = 1
+#     for channel in ctx.guild.channels:
+#         await ctx.send(f'{i} : {channel.name}')
+#         i += 1
+
+# @commands.command()
+# async def clear(ctx):
+#     await ctx.channel.purge(limit=100)
 
 bot = MyBot()
-bot.add_command(test)
+# bot.add_command(test)
+# bot.add_command(clear)
+# bot.add_command(channel)
 
-bot.run('MTM4NTMwNDQyODAzODMyODMyMA.GP31em.GkfgNN_bxsl4KPyuJRwpk9rVLCyIeQJXr6zFpo')
+bot.run(TOKEN)
