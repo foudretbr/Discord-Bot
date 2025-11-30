e3d96373d1d0666c3427b7d4eff1a2712ed6e9f9
foudretbr <panier.ikea@gmail.com>
2025-11-30 21:57:09 +0100

    commit

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,18 +1,25 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord.ext import context
+from discord.ext.commands import Context
 
 class Moderation(commands.Cog, name="moderation"):
     def __init__(self, bot):
         self.bot = bot
 
 
-    @commands.hybrid_command(name="kick", description="Kick an user out of the serve.")
+    @commands.hybrid_command(name="kick", description="Kick an user out of the server.")
     @commands.has_permissions(kick_members=True)
-    @app_commands.describe(user="The user to kick", reason="The reason why the user should be kick")
-    async def kick(self, context: context, user: discord.User, *, reason: str = "Not specified"):
-        """Kick an user from the server """
+    @commands.bot_has_permissions(kick_members=True)
+    @app_commands.describe(user="The user to kick", reason="The reason why the user should be kicked")
+    async def kick(self, context: Context, user: discord.User, *, reason: str = "Not specified"):
+        """
+        Kick an user from the server.
+
+        :param context: The hybrid command context.
+        :param user: The user to kick.
+        :param reason: The reason why the user should be kicked. 
+        """
         member = context.guild.get_member(user.id) or await context.guild.fetch_member(user.id)
         if member.guild_permissions.administrator:
             embed = discord.Embed(description="User has administrator permission.", color=0xE02B2B)
@@ -32,6 +39,51 @@ class Moderation(commands.Cog, name="moderation"):
                 embed = discord.Embed(description="An error occurred while trying to kick the user. Make sure my role is above the role of the user you want to kick.", color=0xE02B2B)
                 await context.send(embed=embed)
 
+    @commands.hybrid_command(name="ban", description="A command for ban a user from the server.")
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    @app_commands.describe(user="The user to ban", reason= "The reason why the user should be banned")
+    async def ban(self, context: Context, user : discord.User, *, reason: str = "The reason why the user should be ban."):
+        """
+        Ban an user from the server.
+        
+        :param context: The hybrid command context.
+        :param user: The user who should be banned.
+        :param reason: The reason why the user should be banned.
+        """
+        member = context.guild.get_member(user.id) or await context.guild.fetch_member(user.id)
+        if member.guild_permissions.administrator:
+            embed = discord.Embed(description="User has administrator permission.", color=0xE02B2B)
+            await context.send(embed=embed)
+        else:
+            try:
+                embed = discord.Embed(description=f"**{member}** was banned by **{context.author}**!",color=0xBEBEFE,)
+                embed.add_field(name="Reason:", value=reason)
+                await context.send(embed=embed)
+                try:
+                    await member.send(f"You were banned from **{context.guild.name}** by **{context.author}** !\nReason: {reason}")
+                except:
+                    pass
+                await member.ban(reason=reason)
+            except:
+                embed = discord.Embed(description="An error occurred while trying to ban the user. Make sure my role is above the role of the user you want to ban.", color=0xE02B2B)
+                await context.send(embed=embed)
+
+    @commands.hybrid_command(name="clear", description="Clear a specified number of messages in the channel.")
+    @commands.has_guild_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    @app_commands.describe(amount="The number of messages to clear")
+    async def clear(self, context: Context, amount: int):
+        """
+        Clear a specified number of messages in the channel.
+        
+        :param context: The hybrid command context.
+        :param amount: The number of messages that should be deleted.
+        """
+        await context.send(f"Deleting {amount} messages...")
+        await context.channel.purge(limit=amount+2) # +2 to include the command message and the "Deleting amount message..." message
+        embed = discord.Embed(description=f"✅ | Cleared {amount} messages !", color=0xBEBEFE)
+        await context.channel.send(embed=embed, delete_after=5)
+
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
-
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import discord
 from discord.ext import commands
 from discord.ext import context
@@ -5,23 +6,86 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 TOKEN = os.getenv('TOKEN')
 PREFIX = os.getenv('PREFIX', '$')
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-    
+
+class LoggingFormatter(logging.Formatter):
+    # Colors in ANSI code, refer to https://talyian.github.io/ansicolors/
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format = format.replace("(black)", self.black + self.bold)
+        format = format.replace("(reset)", self.reset)
+        format = format.replace("(levelcolor)", log_color)
+        format = format.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
+
+
+logger = logging.getLogger("discord_bot")
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+# File handler
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
+)
+file_handler.setFormatter(file_handler_formatter)
+
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=PREFIX, intents=intents)
+        self.logger = logger
 
-    async def on_ready(self):
+    async def load_cogs(self):
         for file in os.listdir('./cogs'):
             if file.endswith('.py'):
-                await self.load_extension(f'cogs.{file[:-3]}')
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f'cogs.{extension}')
+                    self.logger.info(f'Loaded extension: {extension}')
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(
+                        f'Failed to load extension {extension}\n{exception}'
+                    )
 
-        print(f"Bot is ready! Connected to : {self.user}")
+    async def setup_hook(self):
+        self.logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
+        await self.load_cogs()
+
+    async def on_ready(self):
+        self.logger.info(f'{self.user} (ID: {self.user.id} ) is ready.')
 
     async def on_member_join(self, member):
         guild = member.guild
@@ -38,24 +102,5 @@ class MyBot(commands.Bot):
 
         await self.process_commands(message)
 
-# @commands.command()
-# async def test(ctx, arg):
-#     await ctx.channel.send(arg)
-
-# @commands.command()
-# async def channel(ctx):
-#     i = 1
-#     for channel in ctx.guild.channels:
-#         await ctx.send(f'{i} : {channel.name}')
-#         i += 1
-
-# @commands.command()
-# async def clear(ctx):
-#     await ctx.channel.purge(limit=100)
-
 bot = MyBot()
-# bot.add_command(test)
-# bot.add_command(clear)
-# bot.add_command(channel)
-
 bot.run(TOKEN)
