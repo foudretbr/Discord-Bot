5ad1be394124e6d54d886c5ef09535ce55371025
foudretbr <86318354+foudretbr@users.noreply.github.com>
2026-01-22 11:28:59 +0100

    Change basics cogs
    Removed racist command 'nga' and its related functionality from troll.py.

--- cogs/troll.py
+++ cogs/troll.py
@@ -52,46 +52,5 @@ class PexelsAPI(commands.Cog, name="troll"):
                 image_url = photo.original
                 await context.send(image_url)
     
-    @commands.hybrid_command(name="nga", description="A command who return a random image of a black.")
-    @commands.bot_has_permissions(send_messages=True)
-    @app_commands.describe(amount = "The amount of image of black to research.")
-    async def nga(self, context: Context, amount : int):
-        """
-        Command to search image with a racist keyword, to humour, for an amount.
-        
-        :param context: The hybrid command context.
-        :type context: Context
-        :param amount: The amount of images to search in Pexels.
-        :type amount: int
-        """
-        if amount > 4:
-            embed = discord.Embed(description="Amount max is 4 !", color = 0xE02B2B)
-            await context.send(embed = embed)
-            return
-        
-        for i in range(amount):
-            api.search("nigger", page=1, results_per_page=100)
-            photos = api.get_entries()
-            if not photos:
-                embed = discord.Embed(description="No images found !", color = 0xE02B2B)
-                await context.send(embed = embed)
-                return
-            else:
-                photo = random.choice(photos)
-                image_url = photo.original
-                await context.send(image_url)
-
-    @commands.hybrid_command(name="ludo", description="A command who send a ludo emoji")
-    @commands.bot_has_permissions(send_messages=True)
-    @app_commands.describe(amount = "The amount of emoji to send.")
-    async def ludo(self, context : Context, amount : int):
-        """
-        A command to send Ludo emojis
-        """
-        emoji = discord.utils.get(context.guild.emojis, name="ludo_prime")
-        for i in range(amount):
-            await context.send(str(emoji))
-
-
 async def setup(bot):
     await bot.add_cog(PexelsAPI(bot))
